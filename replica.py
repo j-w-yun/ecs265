8e0a130866071df6a914c1ada6496e225080ab2e
Jaewan Yun <jay50@pitt.edu>
2021-11-18 15:16:07 -0800

    add socket client

--- replica.py
+++ replica.py
@@ -1,33 +1,116 @@
-import global_const
+import json
+import string
 
-REPLICA_NUMBER = -1
+# import paho.mqtt.client as mqtt
+import socket_client as mqtt
+print(mqtt)
+
+from global_const import *
 
 
-def broadcast_to_all_nodes(message):
-    print("Broadcasting to All nodes: "+ str(message))
-    global_const.mqttStuff.publish(global_const.topicPrefix + "/prepare", message + " REPLICA "+str(REPLICA_NUMBER))
+class Replica:
+    def __init__(self, role='r', replica_id=1, current_seq=0) -> None:
+        self.role = Role.PRIMARY if role == 'p' else Role.REPLICA
+        self.current_seq = current_seq
+        self.id = replica_id
 
-def get_message(topic, message):
-    ## REQUEST PHASE
-    if topic == global_const.topicPrefix + "/request":
-        print("Request message: "+str(message)+" received from client. Replica " + str(REPLICA_NUMBER) + " does nothing.")
+        self.current_seq = current_seq
+        self.current_phase = ConsensusPhase.IDLE
+        self.prepare_cache = []
+        self.commit_cache = []
 
-    ## PRE-PREPARE PHASE
-    if topic == global_const.topicPrefix + "/pre-prepare":
-        print("Pre-prepare message received. Replica " + str(REPLICA_NUMBER) + " is now in PREPARE PHASE. \nBroadcasting PREPARE to all")
-        input("Press Enter to continue...")
-        broadcast_to_all_nodes(str(message))
+        # MQTT client setup
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+        # self.mqtt_client.connect(MQTT_SERVER_ADDR, MQTT_SERVER_PORT, 60)
+        self.mqtt_client.connect('https://jaewan-yun.com', '5000')
+        self.mqtt_client.loop_forever()
 
-    ## PREPARE PHASE
-    if topic == global_const.topicPrefix + "/prepare":
-        print("Prepare message received from a different Node: "+str(message))
-        list_of_prepare_certs = []
-        if(len(list_of_prepare_certs)==3):
-            print("Going to commit.")
+    def on_connect(self):
+        print("Connected to MQTT server.")
+        self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'terminal')
+        self.broadcast_msg('terminal', "I'm here")
 
-def init_replica(replica_id):
-    global REPLICA_NUMBER
-    REPLICA_NUMBER = replica_id 
-    print("Replica " + str(REPLICA_NUMBER) + " initialized.")
+        if self.role == Role.PRIMARY:
+            self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'client_req')
+            self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'client_req', self.on_client_message)
 
-print("")
+        self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'pre-prepare')
+        self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'pre-prepare', self.on_pre_prepare_message)
+        self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'prepare')
+        self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'prepare', self.on_prepare_message)
+        self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'commit')
+        self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'commit', self.on_commit_message)
+
+    def on_message(self, client, usr_data, msg):
+        pass
+
+    def on_client_message(self, client, usr_data, msg):
+        self.current_phase = ConsensusPhase.PRE_PREPARE
+        self.current_seq += 1
+        self.broadcast_msg('pre-prepare', 'Proposal from primary')
+
+    def on_pre_prepare_message(self, client, usr_data, msg):
+        msg = self.validate_msg(msg)
+        if msg and self.role == Role.REPLICA and msg['role'] == Role.PRIMARY.value and self.current_phase == ConsensusPhase.IDLE:
+                if msg['current_seq'] > self.current_seq:
+                    self.current_seq = msg['current_seq']
+                    self.current_phase = ConsensusPhase.PRE_PREPARE
+                    self.broadcast_msg('prepare', 'Vote for prepare certificate')
+
+    def on_prepare_message(self, client, usr_data, msg):
+        msg = self.validate_msg(msg)
+        if msg and msg['current_seq'] == self.current_seq and self.current_phase == ConsensusPhase.PRE_PREPARE:
+            self.prepare_cache.append(msg)
+
+            if len(self.prepare_cache) + self.role.value > 2 * FAULT_TOLERANCE:
+                self.prepare_cache.clear()
+                self.current_phase = ConsensusPhase.COMMIT
+                self.broadcast_msg('commit', 'Vote for commit')
+
+    def on_commit_message(self, client, usr_data, msg):
+        msg = self.validate_msg(msg)
+        if msg and msg['current_seq'] == self.current_seq and self.current_phase == ConsensusPhase.COMMIT:
+            self.commit_cache.append(msg)
+
+            if len(self.commit_cache) > 2 * FAULT_TOLERANCE:
+                result  = self.execute()
+                if result:
+                    self.commit_cache.clear()
+                    self.current_phase = ConsensusPhase.IDLE
+                    self.broadcast_msg('client', 'Request executed')
+
+    def validate_msg(self, msg) -> bool:
+        try:
+            msg = json.loads(msg.payload)
+            return msg
+        except:
+            return
+
+    def execute(self) -> bool:
+        return True
+
+    def construct_msg(self, content) -> string:
+        msg = {}
+        msg['id'] = self.id
+        msg['role'] = self.role.value
+        msg['current_seq'] = self.current_seq
+        msg['current_phase'] = self.current_phase.value
+
+        if isinstance(content, dict):
+            for key, value in content.items():
+                msg[key] = value
+        else:
+            msg['meta'] = str(content)
+
+        return json.dumps(msg)
+
+
+    def broadcast_msg(self, topic, msg):
+        msg = self.construct_msg(msg)
+        self.mqtt_client.publish(MQTT_TOPIC_PREFIX + topic, msg)
+
+
+if __name__ == '__main__':
+    primary = Replica('r', 4)
---
+++ socket_client/client.py
@@ -0,0 +1,62 @@
+import asyncio
+import socketio
+import time
+
+
+class Client:
+    def __init__(self, namespace='/pbft'):
+        self.namespace = namespace
+
+        self.host = None
+        self.port = None
+        self.sio = None
+
+        self.on_connect = None
+        self.on_disconnect = None
+        self.on_message = None
+
+        self.topics = dict()
+
+    def subscribe(self, topic):
+        self.topics[topic] = None
+
+    def message_callback_add(self, topic, callback):
+        self.topics[topic] = callback
+
+    def publish(self, topic, message):
+        self.sio.emit(topic, message, namespace=self.namespace)
+
+    def _initialize_socket(self):
+        client = self
+
+        @self.sio.event(namespace=self.namespace)
+        def connect():
+            if self.on_connect:
+                self.on_connect()
+                self.publish('test', 'testing hello world!')
+
+        @self.sio.event(namespace=self.namespace)
+        def disconnect():
+            if self.on_disconnect:
+                self.on_disconnect()
+
+        class Namespace(socketio.ClientNamespace):
+            def trigger_event(self, name, *args):
+                print(f"event={name}, payload={args}")
+                if name in client.topics and client.topics[name]:
+                    client.topics[name](*args)
+
+        self.sio.register_namespace(Namespace(self.namespace))
+
+    def connect(self, host, port):
+        self.sio = socketio.Client()
+        self._initialize_socket()
+
+        self.host = host
+        self.port = port
+        self.sio.connect(f"{self.host}:{self.port}", namespaces=[self.namespace])
+
+    def loop_forever(self):
+        while True:
+            time.sleep(1)
+            # print('loop')
