88b5329b00d2edfd41ba5abbcae9838d011c68b5
theForerunner <4runn3rcn@gmail.com>
2021-11-22 01:36:44 -0800

    updated the data structure of log, added reply history, and request timestamp check

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # ecs265
 
+## Test Method
+* Open MQTT Explorer, choose test.mosquitto.org on the left side of the connection page and change the topic to `ecs265/pbft/#`
+* run four instances of replica by using the following command with replica_id from 0 to 3 (You should see messages in `ecs265/pbft/terminal`)
+  ```
+  python replica.py replica_id
+  ```
+* Publish a client request with topic `ecs265/pbft/client_req` with the following format
+  ```
+  {"o":"Operation requested","t":1637563592,"c":0}
+  ```
+* You should be able to see messages of different topics and everyone finally commits and replies 
 ## Message Types
 
 As messages with different types are sent into different topics, there is no need to contain the message type in the message content.
--- replica.py
+++ replica.py
@@ -16,9 +16,9 @@ class Replica:
         self.update_role()
 
         self.current_phase = ConsensusPhase.IDLE
-        self.log = []
-        self.prepare_bit_mask = [0] * NODE_TOTAL_NUMBER
-        self.commit_bit_mask = [0] * NODE_TOTAL_NUMBER
+        self.log = {}
+        self.reply_history = {}
+
         self.client_req = ''
         self.client_req_digest = ''
         self.client_req_dict ={}
@@ -62,89 +62,99 @@ class Replica:
 
                 # Construct and broadcast the pre-prepare message as well as appending it to log
                 pre_prepare_msg = self.construct_msg(if_proposal=True)
-                self.log.append(pre_prepare_msg)
+
+                # Initiate log and append pre-prepare message
+                self.initiate_log()
+                self.log[self.current_seq]['pre-prepare'][self.id] = pre_prepare_msg
+
                 self.broadcast_msg('pre-prepare', pre_prepare_msg)
 
     def on_pre_prepare_message(self, client, usr_data, msg):
         if self.role == Role.REPLICA:
-            pre_prepare_msg_dict = self.validate_proposal(msg)
-            if pre_prepare_msg_dict and self.current_phase == ConsensusPhase.IDLE:
-                # Append pre-prepare message to log
-                self.log.append(msg)
-
+            pre_prepare_msg = self.validate_proposal(msg)
+            if pre_prepare_msg and self.current_phase == ConsensusPhase.IDLE:
                 # Update local client request info
-                self.client_req = pre_prepare_msg_dict['m']
-                self.client_req_digest = pre_prepare_msg_dict['d']
+                self.client_req = pre_prepare_msg['m']
+                self.client_req_digest = pre_prepare_msg['d']
                 self.client_req_dict = json.loads(self.client_req)
 
                 # Update sequence number and consensus phase
-                self.current_seq = pre_prepare_msg_dict['n']
+                self.current_seq = pre_prepare_msg['n']
                 self.current_phase = ConsensusPhase.PREPARE
 
-                # Construct and broadcast the prepare message as well as appending it to log
+                # Append pre-prepare message to log
+                self.initiate_log()
+                self.log[self.current_seq]['pre-prepare'][pre_prepare_msg['i']] = msg.payload.decode('utf-8')
+
+                # Reject request with lower timestamp than last replied request to ensure exactly-once semantics
+                try:
+                    if self.client_req_dict['t'] <= self.reply_history[self.client_req_dict['c']]['t']:
+                        return
+                except KeyError:
+                    pass
+
+                # Construct and broadcast the prepare message
                 prepare_msg = self.construct_msg()
-                self.log.append(prepare_msg)
                 self.broadcast_msg('prepare', prepare_msg)
+        else:
+            self.current_phase = ConsensusPhase.PREPARE
 
     def on_prepare_message(self, client, usr_data, msg):
-        prepare_msg_dict = self.validate_msg(msg)
-        if prepare_msg_dict and self.current_phase == ConsensusPhase.PREPARE:
-            try:
-                # Update prepare bit mask: if a prepare message from replica i is accepted, self.prepare_bit_mask[i] = 1 else it is remained 0
-                self.prepare_bit_mask[prepare_msg_dict['i']] = 1
+        prepare_msg = self.validate_msg(msg)
+        if prepare_msg and self.current_phase == ConsensusPhase.PREPARE:
+            # Append prepare message to log
+            self.log[self.current_seq]['prepare'][prepare_msg['i']] = msg.payload.decode('utf-8')
 
-                # Append prepare message to log
-                self.log.append(msg)
-            except IndexError:
-                pass
-
-            if len(list(filter(lambda x : x > 0, self.prepare_bit_mask))) + self.role.value > 2 * FAULT_TOLERANCE:
-                # Reset prepare bit mask
-                self.prepare_bit_mask = [0] * NODE_TOTAL_NUMBER
-
+            if len(self.log[self.current_seq]['prepare'].keys()) + self.role.value > 2 * FAULT_TOLERANCE:
                 # Update consensus phase
                 self.current_phase = ConsensusPhase.COMMIT
 
-                # Construct and broadcast the prepare message as well as appending it to log
+                # Construct and broadcast the prepare message
                 commit_msg = self.construct_msg()
-                self.log.append(commit_msg)
                 self.broadcast_msg('commit', commit_msg)
 
     def on_commit_message(self, client, usr_data, msg):
-        commit_msg_dict = self.validate_msg(msg)
-        if commit_msg_dict and self.current_phase == ConsensusPhase.COMMIT:
-            try:
-                # Update commit bit mask: if a commit message from replica i is accepted, self.commit_bit_mask[i] = 1 else it is remained 0
-                self.commit_bit_mask[commit_msg_dict['i']] = 1
+        commit_msg = self.validate_msg(msg)
+        if commit_msg and self.current_phase == ConsensusPhase.COMMIT:
+            # Append commit message to log
+            self.log[self.current_seq]['commit'][commit_msg['i']] = msg.payload.decode('utf-8')
 
-                # Append commit message to log
-                self.log.append(msg)
-            except IndexError:
-                pass
-
-            if len(list(filter(lambda x : x > 0, self.commit_bit_mask))) > 2 * FAULT_TOLERANCE:
+            if len(self.log[self.current_seq]['commit'].keys()) > 2 * FAULT_TOLERANCE:
                 result  = self.execute(self.client_req_dict['o'])
                 if result:
-                    # Reset prepare bit mask
-                    self.commit_bit_mask = [0] * NODE_TOTAL_NUMBER
-
                     # Update consensus phase
                     self.current_phase = ConsensusPhase.IDLE
 
                     # Construct and broadcast the prepare message as well as appending it to log
                     reply_msg = self.construct_reply('Request executed succeeded' if result else 'Request executed failed')
-                    self.broadcast_msg('client', reply_msg)
+                    # Append the reply message to reply history
+                    self.append_reply_history(reply_msg)
+                    self.broadcast_msg('reply', reply_msg)
 
     def execute(self, operation) -> bool:
+        with open('log_sample/log_{}.json'.format(self.id), 'w', encoding='utf-8') as fw:
+            json.dump(self.log, fw)
         return True
 
     def broadcast_msg(self, topic, msg):
         self.mqtt_client.publish(MQTT_TOPIC_PREFIX + topic, msg)
 
+    def initiate_log(self):
+        self.log[self.current_seq] = {}
+        self.log[self.current_seq]['v'] = self.current_view
+        self.log[self.current_seq]['pre-prepare'] = {}
+        self.log[self.current_seq]['prepare'] = {}
+        self.log[self.current_seq]['commit'] = {}
+
+    def append_reply_history(self, msg):
+        self.reply_history[self.client_req_dict['c']] = {}
+        self.reply_history[self.client_req_dict['c']]['t'] = self.client_req_dict['t']
+        self.reply_history[self.client_req_dict['c']]['r'] = msg
+
     def validate_client_req(self, msg) -> dict:
         try:
-            msg = json.loads(msg.payload)
-            if not (msg['o'] and msg['t'] and msg['c']):
+            msg = json.loads(msg.payload.decode('utf-8'))
+            if not all (key in msg for key in ['o', 't', 'c']):
                 msg = {}
             return msg
         except (json.decoder.JSONDecodeError, KeyError):
@@ -152,18 +162,18 @@ class Replica:
 
     def validate_proposal(self, msg) -> dict:
         try:
-            msg = json.loads(msg.payload)
+            msg = json.loads(msg.payload.decode('utf-8'))
             val_mac = self.get_mac(json.dumps({key : value for key, value in msg.items() if key in MSG_DIGEST_KEYS}), msg['i'])
             val_digest = self.get_digest(msg['m'])
             if not (msg['mac'] == val_mac and msg['d'] == val_digest and msg['v'] == self.current_view and msg['n'] > self.current_seq):
                 msg = {}
             return msg
-        except (json.decoder.JSONDecodeError, KeyError) as e:
+        except (json.decoder.JSONDecodeError, KeyError):
             return {}
 
     def validate_msg(self, msg) -> dict:
         try:
-            msg = json.loads(msg.payload)
+            msg = json.loads(msg.payload.decode('utf-8'))
             val_mac = self.get_mac(json.dumps({key : value for key, value in msg.items() if key in MSG_DIGEST_KEYS}), msg['i'])
             if  not (msg['mac'] == val_mac and msg['d'] == self.client_req_digest and msg['v'] == self.current_view and msg['n'] == self.current_seq):
                 msg = {}
