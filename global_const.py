8bbdbfdd7df871e5116dcedcaf606f8ca1f289e8
Jaewan Yun <jay50@pitt.edu>
2021-11-26 23:45:16 -0800

    ported socketio

--- global_const.py
+++ global_const.py
@@ -1,11 +1,17 @@
 from enum import Enum
 
 
-MQTT_SERVER_ADDR = 'test.mosquitto.org'
-MQTT_SERVER_PORT = 1883
-MQTT_TOPIC_PREFIX = 'ecs265/pbft/'
+# MQTT_SERVER_ADDR = 'test.mosquitto.org'
+# MQTT_SERVER_PORT = 1883
+# MQTT_TOPIC_PREFIX = 'ecs265/pbft/'
+MQTT_SERVER_ADDR = 'https://jaewan-yun.com'
+MQTT_SERVER_PORT = '5000'
+MQTT_TOPIC_PREFIX = ''
 
+NODE_TOTAL_NUMBER = 4
 FAULT_TOLERANCE = 1
+
+MSG_DIGEST_KEYS = ['i', 'n', 'v', 'd']
 
 class ConsensusPhase(Enum):
     IDLE = 0
@@ -13,7 +19,13 @@ class ConsensusPhase(Enum):
     PREPARE = 2
     COMMIT = 3
     VIEWCHANGE = 4
+    NEW_VIEW = 5
 
 class Role(Enum):
     REPLICA = 0
     PRIMARY = 1
+
+class State(Enum):
+    READY = 0
+    WAITING = 1
+    CHANGING = 2
--- replica.py
+++ replica.py
@@ -1,37 +1,76 @@
+import hashlib
 import json
 import string
+import sys
+from threading import Thread,Event,Timer
 
-import paho.mqtt.client as mqtt
+# import paho.mqtt.client as mqtt
+import socket_client as mqtt
+print(mqtt)
 
 from global_const import *
 
 
-class Replica:
-    def __init__(self, role='r', replica_id=1, current_seq=0) -> None:
-        self.role = Role.PRIMARY if role == 'p' else Role.REPLICA
-        self.current_seq = current_seq
-        self.id = replica_id
+class WaitTimer:
+    def __init__(self,function):
+        Thread.__init__(self)
+        self.interval = 60
+        self.function = function
+        self.finished = Event()
+        self.resetted = True
 
+    def cancel(self):
+        self.finished.set()
+
+    def run(self):
+        while self.resetted:
+            self.resetted = False
+            self.finished.wait(self.interval)
+        if not self.finished.isSet():
+            self.function()
+        self.finished.set()
+
+    def reset(self):
+        self.resetted = True
+        self.finished.set()
+        self.finished.clear()
+
+
+class Replica:
+    # def __init__(self, role='r', replica_id=1, current_seq=0) -> None:
+    def __init__(self, replica_id=0, current_seq=0, current_view=0) -> None:
+        self.id = replica_id
         self.current_seq = current_seq
+        self.current_view = current_view
+        self.state = State.READY
+        self.timer = WaitTimer(self.init_view_change)
+        self.requests = 0
+
+        self.update_role()
+
         self.current_phase = ConsensusPhase.IDLE
-        self.prepare_cache = []
-        self.commit_cache = []
+        self.log = {}
+        self.reply_history = {}
+
+        self.client_req = ''
+        self.client_req_digest = ''
+        self.client_req_dict ={}
 
         # MQTT client setup
         self.mqtt_client = mqtt.Client()
         self.mqtt_client.on_connect = self.on_connect
         self.mqtt_client.on_message = self.on_message
-        self.mqtt_client.connect(MQTT_SERVER_ADDR, MQTT_SERVER_PORT, 60)
+        self.mqtt_client.connect(MQTT_SERVER_ADDR, MQTT_SERVER_PORT)
         self.mqtt_client.loop_forever()
 
-    def on_connect(self, client, userdata, flag, rc):
+    def on_connect(self):
         print("Connected to MQTT server.")
         self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'terminal')
-        self.broadcast_msg('terminal', "I'm here")
+        self.broadcast_msg('terminal', '{} with id {} has connected to the server.'.format('Primary' if self.role == Role.PRIMARY else 'Replica', self.id))
+        self.broadcast_msg('assign_id', json.dumps({'id': self.id}))
 
-        if self.role == Role.PRIMARY:
-            self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'client_req')
-            self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'client_req', self.on_client_message)
+        self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'client_req')
+        self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'client_req', self.on_client_message)
 
         self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'pre-prepare')
         self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'pre-prepare', self.on_pre_prepare_message)
@@ -40,74 +79,225 @@ class Replica:
         self.mqtt_client.subscribe(MQTT_TOPIC_PREFIX + 'commit')
         self.mqtt_client.message_callback_add(MQTT_TOPIC_PREFIX + 'commit', self.on_commit_message)
 
-    def on_message(self, client, usr_data, msg):
+    def on_message(self, msg):
         pass
 
-    def on_client_message(self, client, usr_data, msg):
-        self.current_phase = ConsensusPhase.PRE_PREPARE
-        self.current_seq += 1
-        self.broadcast_msg('pre-prepare', 'Proposal from primary')
+    def on_client_message(self, msg):
+        print('on_client_message', msg)
 
-    def on_pre_prepare_message(self, client, usr_data, msg):
-        msg = self.validate_msg(msg)
-        if msg and self.role == Role.REPLICA and msg['role'] == Role.PRIMARY.value and self.current_phase == ConsensusPhase.IDLE: 
-                if msg['current_seq'] > self.current_seq:
-                    self.current_seq = msg['current_seq']
-                    self.current_phase = ConsensusPhase.PRE_PREPARE
-                    self.broadcast_msg('prepare', 'Vote for prepare certificate')
+        if self.role == Role.PRIMARY:
+            self.client_req_dict = self.validate_client_req(msg)
+            if self.client_req_dict:
+                # Update local client request info
+                self.client_req = json.dumps(self.client_req_dict)
+                self.client_req_digest = self.get_digest(self.client_req)
 
-    def on_prepare_message(self, client, usr_data, msg):
-        msg = self.validate_msg(msg)
-        if msg and msg['current_seq'] == self.current_seq and self.current_phase == ConsensusPhase.PRE_PREPARE:
-            self.prepare_cache.append(msg)
+                # Update sequence number and consensus phase
+                self.current_seq += 1
+                self.current_phase = ConsensusPhase.PRE_PREPARE
 
-            if len(self.prepare_cache) + self.role.value > 2 * FAULT_TOLERANCE:
-                self.prepare_cache.clear()
-                self.current_phase = ConsensusPhase.COMMIT
-                self.broadcast_msg('commit', 'Vote for commit')
+                # Construct and broadcast the pre-prepare message as well as appending it to log
+                pre_prepare_msg = self.construct_msg(if_proposal=True)
 
-    def on_commit_message(self, client, usr_data, msg):
-        msg = self.validate_msg(msg)
-        if msg and msg['current_seq'] == self.current_seq and self.current_phase == ConsensusPhase.COMMIT:
-            self.commit_cache.append(msg)
+                # Initiate log and append pre-prepare message
+                self.initiate_log()
+                self.log[self.current_seq]['pre-prepare'][self.id] = pre_prepare_msg
 
-            if len(self.commit_cache) > 2 * FAULT_TOLERANCE:
-                result  = self.execute()
-                if result:
-                    self.commit_cache.clear()
-                    self.current_phase = ConsensusPhase.IDLE
-                    self.broadcast_msg('client', 'Request executed')
+                self.broadcast_msg('pre-prepare', pre_prepare_msg)
+                self.requests += 1
+        else: # Get into Waiting state
+            if self.state == State.READY:
+                self.state == State.WAITING
+                self.timer.run()
 
-    def validate_msg(self, msg) -> bool:
-        try:
-            msg = json.loads(msg.payload)
-            return msg
-        except:
+    def on_pre_prepare_message(self, msg):
+        print('on_pre_prepare_message', msg)
+
+        if self.state == State.CHANGING:
             return
+        if self.state == State.WAITING:
+            self.timer.reset()
+            self.timer.run()
+        if self.role == Role.REPLICA:
+            pre_prepare_msg = self.validate_proposal(msg)
+            if pre_prepare_msg and self.current_phase == ConsensusPhase.IDLE:
+                # Update local client request info
+                self.client_req = pre_prepare_msg['m']
+                self.client_req_digest = pre_prepare_msg['d']
+                self.client_req_dict = json.loads(self.client_req)
 
-    def execute(self) -> bool:
+                # Update sequence number and consensus phase
+                self.current_seq = pre_prepare_msg['n']
+                self.current_phase = ConsensusPhase.PREPARE
+
+                # Append pre-prepare message to log
+                self.initiate_log()
+                self.log[self.current_seq]['pre-prepare'][pre_prepare_msg['i']] = msg
+
+                # Reject request with lower timestamp than last replied request to ensure exactly-once semantics
+                try:
+                    if self.client_req_dict['t'] <= self.reply_history[self.client_req_dict['c']]['t']:
+                        return
+                except KeyError:
+                    pass
+
+                # Construct and broadcast the prepare message
+                prepare_msg = self.construct_msg()
+                self.broadcast_msg('prepare', prepare_msg)
+                self.requests += 1
+        else:
+            self.current_phase = ConsensusPhase.PREPARE
+
+    def on_prepare_message(self, msg):
+        print('on_prepare_message', msg)
+
+        if self.state == State.CHANGING:
+            return
+        prepare_msg = self.validate_msg(msg)
+        if prepare_msg and self.current_phase == ConsensusPhase.PREPARE:
+            # Append prepare message to log
+            self.log[self.current_seq]['prepare'][prepare_msg['i']] = msg
+
+            if len(self.log[self.current_seq]['prepare'].keys()) + self.role.value > 2 * FAULT_TOLERANCE:
+                # Update consensus phase
+                self.current_phase = ConsensusPhase.COMMIT
+
+                # Construct and broadcast the prepare message
+                commit_msg = self.construct_msg()
+                self.broadcast_msg('commit', commit_msg)
+
+    def on_commit_message(self, msg):
+        print('on_commit_message', msg)
+
+        if self.state == State.CHANGING:
+            return
+        commit_msg = self.validate_msg(msg)
+        if commit_msg['i'] == self.current_view:
+            self.timer.reset()
+            self.timer.run()
+        if commit_msg and self.current_phase == ConsensusPhase.COMMIT:
+            # Append commit message to log
+            self.log[self.current_seq]['commit'][commit_msg['i']] = msg
+
+            if len(self.log[self.current_seq]['commit'].keys()) > 2 * FAULT_TOLERANCE:
+                result  = self.execute(self.client_req_dict['o'])
+                if result:
+                    # Update consensus phase
+                    self.current_phase = ConsensusPhase.IDLE
+
+                    # Construct and broadcast the prepare message as well as appending it to log
+                    reply_msg = self.construct_reply('Request executed succeeded' if result else 'Request executed failed')
+                    # Append the reply message to reply history
+                    self.append_reply_history(reply_msg)
+                    self.broadcast_msg('reply', reply_msg)
+
+    def execute(self, operation) -> bool:
+        with open('log_sample/log_{}.json'.format(self.id), 'w', encoding='utf-8') as fw:
+            json.dump(self.log, fw)
+        self.requests -= 1
+        if self.requests == 0:
+            self.state = State.READY
         return True
 
-    def construct_msg(self, content) -> string:
-        msg = {}
-        msg['id'] = self.id
-        msg['role'] = self.role.value
-        msg['current_seq'] = self.current_seq
-        msg['current_phase'] = self.current_phase.value
+    def broadcast_msg(self, topic, msg):
+        self.mqtt_client.publish(MQTT_TOPIC_PREFIX + topic, msg)
 
-        if isinstance(content, dict):
-            for key, value in content.items():
-                msg[key] = value
-        else:
-            msg['meta'] = str(content)
+    def initiate_log(self):
+        self.log[self.current_seq] = {}
+        self.log[self.current_seq]['v'] = self.current_view
+        self.log[self.current_seq]['pre-prepare'] = {}
+        self.log[self.current_seq]['prepare'] = {}
+        self.log[self.current_seq]['commit'] = {}
+
+    def append_reply_history(self, msg):
+        self.reply_history[self.client_req_dict['c']] = {}
+        self.reply_history[self.client_req_dict['c']]['t'] = self.client_req_dict['t']
+        self.reply_history[self.client_req_dict['c']]['r'] = msg
+
+    def validate_client_req(self, msg) -> dict:
+        try:
+            msg = json.loads(msg)
+            if not all (key in msg for key in ['o', 't', 'c']):
+                msg = {}
+            return msg
+        except (json.decoder.JSONDecodeError, KeyError):
+            return {}
+
+    def validate_proposal(self, msg) -> dict:
+        try:
+            msg = json.loads(msg)
+            val_mac = self.get_mac(json.dumps({key : value for key, value in msg.items() if key in MSG_DIGEST_KEYS}), msg['i'])
+            val_digest = self.get_digest(msg['m'])
+            if not (msg['mac'] == val_mac and msg['d'] == val_digest and msg['v'] == self.current_view and msg['n'] > self.current_seq):
+                msg = {}
+            return msg
+        except (json.decoder.JSONDecodeError, KeyError):
+            return {}
+
+    def validate_msg(self, msg) -> dict:
+        try:
+            msg = json.loads(msg)
+            val_mac = self.get_mac(json.dumps({key : value for key, value in msg.items() if key in MSG_DIGEST_KEYS}), msg['i'])
+            if  not (msg['mac'] == val_mac and msg['d'] == self.client_req_digest and msg['v'] == self.current_view and msg['n'] == self.current_seq):
+                msg = {}
+            return msg
+        except (json.decoder.JSONDecodeError, KeyError):
+            return {}
+
+    def construct_msg(self, if_proposal=False) -> string:
+        msg = {}
+        msg['v'] = self.current_view
+        msg['n'] = self.current_seq
+        msg['d'] = self.client_req_digest
+        msg['i'] = self.id
+        msg['mac'] = self.get_mac(json.dumps({key : value for key, value in msg.items() if key in MSG_DIGEST_KEYS}), self.id) # Currently using replica id as the parameter for key in the get_mac()
+        if if_proposal:
+            msg['m'] = self.client_req
 
         return json.dumps(msg)
 
+    def construct_reply(self, result_str) ->string:
+        msg = {}
+        msg['v'] = self.current_view
+        msg['t'] = self.client_req_dict['t']
+        msg['c'] = self.client_req_dict['c']
+        msg['i'] = self.id
+        msg['r'] = result_str
 
-    def broadcast_msg(self, topic, msg):
-        msg = self.construct_msg(msg)
-        self.mqtt_client.publish(MQTT_TOPIC_PREFIX + topic, msg)
+        return json.dumps(msg)
+
+    def get_digest(self, content) -> string:
+        # Encoding content using md5 hash
+        content = hashlib.md5(content.encode())
+        # Return as a str object
+        content = content.hexdigest()
+        return content
+        # In case of using only the 10 least bytes as the paper did, comment out previous line, and uncomment following one
+        # return content[:10]
+
+    def get_mac(self, msg, key) -> string:
+        # mac = md5(msg, key)
+        # Encoding msg using md5 hash
+        msg = hashlib.md5(msg.encode())
+        # Update msg with key
+        msg.update(key.encode())
+        # Return as a str object
+        msg = msg.hexdigest()
+        return msg
+        # In case of using only the 10 least bytes as the paper did, comment out previous line, and uncomment following one
+        # return msg[:10]
+
+    def update_role(self): # Can be used in view change to update a replica's role
+        self.role = Role.PRIMARY if self.id == self.current_view % NODE_TOTAL_NUMBER else Role.REPLICA
+
+    def init_view_change(self):
+        if self.state == State.WAITING:
+            self.state = State.CHANGING
+            self.current_phase = ConsensusPhase.VIEWCHANGE
+            # send view-change message
 
 
 if __name__ == '__main__':
-    primary = Replica('r', 4)
+    replica_id = int(sys.argv[1])
+    print('replica_id', replica_id)
+    primary = Replica(replica_id=replica_id)
