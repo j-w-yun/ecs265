c19241c3aef9e4d8bc69d98e006f24b109177d0c
theForerunner <4runn3rcn@gmail.com>
2021-12-10 10:33:03 -0800

    first runnable version of view change

--- global_const.py
+++ global_const.py
@@ -8,24 +8,22 @@ MQTT_SERVER_ADDR = 'https://jaewan-yun.com'
 MQTT_SERVER_PORT = '5000'
 MQTT_TOPIC_PREFIX = ''
 
-NODE_TOTAL_NUMBER = 4
+NODE_TOTAL_NUMBER = 5
 FAULT_TOLERANCE = 1
 
 MSG_DIGEST_KEYS = ['i', 'n', 'v', 'd']
+VIEW_CHANGE_MSG_DIGEST_KEYS = ['v', 'n', 'P', 'i']
+NEW_VIEW_MSG_DIGEST_KEYS = ['v', 'V', 'O', 'i']
 
 class ConsensusPhase(Enum):
     IDLE = 0
     PRE_PREPARE = 1
     PREPARE = 2
     COMMIT = 3
-    VIEWCHANGE = 4
-    NEW_VIEW = 5
+    WAIT = 4
+    VIEW_CHANGE = 5
+    NEW_VIEW = 6
 
 class Role(Enum):
     REPLICA = 0
     PRIMARY = 1
-
-class State(Enum):
-    READY = 0
-    WAITING = 1
-    CHANGING = 2
